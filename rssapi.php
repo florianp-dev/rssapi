2b3f78816e20b8b36be3595705450188a32c9f95
Florian POUCHELET <florian.pouchelet@etu.univ-rouen.fr>
2016-03-27 19:01:19 +0200

    fix(unmarshal) : does not work if flow is not well formed

--- rssapi.php
+++ rssapi.php
@@ -3,12 +3,18 @@
 class RSSAPI {
 
 	public function unmarshal($flux_url) {
+		// Verify integrity of $flux_url
 		if (!is_string($flux_url)) {
 			throw new InvalidArgumentException('Argument must be a string');
 		} elseif (!filter_var($flux_url, FILTER_VALIDATE_URL)) {
 			throw new InvalidArgumentException('URL is not well formed');
 		}
-
+		// Verify if it is well formed XML
+		libxml_use_internal_errors(true);
+		if (!simplexml_load_file($flux_url)) {
+			throw new DOMException('File is not well formed');
+		}
+		
 		$rss = new SimpleXMLElement($flux_url, 0, true);
 
 		// Title of RSS document
