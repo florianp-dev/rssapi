fc950ad82aa2b8dcdbd7303c08e57e502aa4f152
Florian POUCHELET <florian.pouchelet@etu.univ-rouen.fr>
2016-03-27 19:09:34 +0200

    refactor(unmarshal) : add function to check conditions before unserialize flow

--- rssapi.php
+++ rssapi.php
@@ -3,18 +3,6 @@
 class RSSAPI {
 
 	public function unmarshal($flux_url) {
-		// Verify integrity of $flux_url
-		if (!is_string($flux_url)) {
-			throw new InvalidArgumentException('Argument must be a string');
-		} elseif (!filter_var($flux_url, FILTER_VALIDATE_URL)) {
-			throw new InvalidArgumentException('URL is not well formed');
-		}
-		// Verify if it is well formed XML
-		libxml_use_internal_errors(true);
-		if (!simplexml_load_file($flux_url)) {
-			throw new DOMException('File is not well formed');
-		}
-		
 		$rss = new SimpleXMLElement($flux_url, 0, true);
 
 		// Title of RSS document
@@ -33,5 +21,19 @@ class RSSAPI {
 		}
 
 		return $unmarshalled;
+	}
+
+	private function check_integrity($to_check) {
+		// Verify integrity of $flux_url
+		if (!is_string($to_check)) {
+			throw new InvalidArgumentException('Argument must be a string');
+		} elseif (!filter_var($to_check, FILTER_VALIDATE_URL)) {
+			throw new InvalidArgumentException('URL is not well formed');
+		}
+		// Verify if it is well formed XML
+		libxml_use_internal_errors(true);
+		if (!simplexml_load_file($to_check)) {
+			throw new DOMException('File is not well formed');
+		}
 	}
 }
